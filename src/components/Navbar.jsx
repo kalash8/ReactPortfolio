19a2211852a6f4a1c7d3f9c8ed4981f3c4e0b21f
kalash8 <67334628+kalash8@users.noreply.github.com>
2024-08-11 00:54:16 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,14 +11,14 @@ const Navbar = () => {
   const [toogle, setToogle] = useState('');
 
   return (
-    <nav className={`${styles.paddingX} w-full flex justify-center item-center py-5 fixed top-0 z-20 bg-[#0000009f]`}>
+    <nav className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-[#0000009f]`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to="/" className="flex items-center gap-2" onClick={() => {
           setActive("");
           window.scrollTo(0, 0);
         }}>
 {/*           <img src={logo} alt="logo" className="w-12 h-12 object-contain"/> */}
-{/*           <p className="text-white text-[22px] font-bold cursor-pointer flex">Kalash Telkar</p> */}
+          <p className="text-white text-[22px] font-bold cursor-pointer flex">Portfolio</p>
           </Link>
           
         <ul className="list-none hidden sm:flex flex-row gap-10">
