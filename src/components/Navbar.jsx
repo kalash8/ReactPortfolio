6359fc71bee3c947bd79867d78cbc1ed2e56e366
kalash8 <67334628+kalash8@users.noreply.github.com>
2024-08-11 00:52:40 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const [toogle, setToogle] = useState('');
 
   return (
-    <nav className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-[#0000009f]`}>
+    <nav className={`${styles.paddingX} w-full flex justify-center item-center py-5 fixed top-0 z-20 bg-[#0000009f]`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to="/" className="flex items-center gap-2" onClick={() => {
           setActive("");
